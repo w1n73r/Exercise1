93d4924e4655b7bb58b2481658a04b485268f313
w1n73r <christophwinter0@gmail.com>
2016-10-23 12:57:59 +0200

    new class HelloUser

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 
  */
@@ -13,6 +17,18 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2016 - how are you? :)");
+		System.out.print("Please enter your name: ");
+		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+		HelloUser user;
+		try{
+			String name = buf.readLine();
+			user= new HelloUser(name);
+		}
+		catch (IOException ioe){
+			System.err.println("Error!");
+			return;
+		}
+		user.greetUser();
 	}
 
 }
